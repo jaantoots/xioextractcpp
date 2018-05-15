43b2d02cfb9bc7fd43ba7e9e2162034037cbbec5
Jaan Toots <jaantoots@gmail.com>
2018-05-15 23:00:34 +0100

    Dry code and fix endianness

--- osc.cpp
+++ osc.cpp
@@ -1,56 +1,67 @@
 #include <cstdint>
 #include <cassert>
+#include <cmath>
 #include <string>
 #include <algorithm>
 
-#define OSC_INT_WIDTH 4
-#define OSC_FLOAT_WIDTH 4
-#define OSC_STRING_MOD 4
-#define OSC_BLOB_MOD 4
-#define NTP_DELTA 2208988800
+#define OSC_MOD 4
+#define NTP_DELTA 2208988800ll
+
+template<class T, class Iter>
+T get_from_c (Iter &first, const Iter &last) {
+  // Get OSC_MOD chars as type T
+  assert(last - first >= OSC_MOD);
+  union mix32_t {
+    T t;
+    uint32_t u;
+    char c[OSC_MOD];
+  } m;
+  copy_n(first, OSC_MOD, m.c);
+  first += OSC_MOD;
+  m.u = be32toh(m.u);
+  return m.t;
+}
 
 template<class Iter>
 int32_t get_int32 (Iter &first, const Iter &last) {
-  assert(last - first >= OSC_INT_WIDTH);
-  int32_t int32 = *first << 24 | *(++first) << 16 | *(++first) << 8 | *(++first);
-  ++first;
-  return int32;
+  // Get signed 32-bit integer from chars
+  return get_from_c<int32_t>(first, last);
 }
 
 template<class Iter>
 uint32_t get_uint32 (Iter &first, const Iter &last) {
-  assert(last - first >= OSC_INT_WIDTH);
-  uint32_t int32 = *first << 24 | *(++first) << 16 | *(++first) << 8 | *(++first);
-  ++first;
-  return int32;
+  // Get unsigned 32-bit integer from chars
+  return get_from_c<uint32_t>(first, last);
 }
 
+struct timetag {
+  long long secs;
+  long long frac;
+};
+
 template<class Iter>
-double get_time (Iter &first, const Iter &last) {
-  uint32_t secs = get_uint(first, last);
-  uint32_t fsecs = get_uint(first, last);
-  secs -= NTP_DELTA; // NTP time starts at 1 Jan 1900
-  return (double) secs + (double) fsecs / (1ul << 32);
+timetag get_time (Iter &first, const Iter &last) {
+  // Get timestamp from unsigned 64-bit NTP timestamp
+  uint32_t secs = get_uint32(first, last);
+  uint32_t frac = get_uint32(first, last);
+  timetag t = { secs, frac };
+  t.secs -= NTP_DELTA; // NTP time starts at 1 Jan 1900
+  return t;
 }
 
 template<class Iter>
 float get_float (Iter &first, const Iter &last) {
+  // Get IEEE-754 32 bit float
   assert(std::numeric_limits<float>::is_iec559);
-  assert(last - first >= OSC_FLOAT_WIDTH);
-  union mix32_t {
-    float f;
-    char c[OSC_FLOAT_WIDTH];
-  } m;
-  for (int i = 0; i < OSC_FLOAT_WIDTH; ++i)
-    m.c[i] = *(first++);
-  return m.f;
+  return get_from_c<float>(first, last);
 }
 
 template<class Iter>
 std::string get_string (Iter &first, const Iter &last) {
+  // Get a \0 terminated and padded string
   Iter end = std::find(first, last, '\0');
   std::string str(first, end);
-  end += ((first - end) % OSC_STRING_MOD) + OSC_STRING_MOD;
+  end += ((first - end) % OSC_MOD) + OSC_MOD;
   assert(end <= last);
   first = end;
   return str;
@@ -58,19 +69,20 @@ std::string get_string (Iter &first, const Iter &last) {
 
 template<class Iter>
 std::string get_blob (Iter &first, const Iter &last) {
+  // Get arbitrary length binary blob
   long count = get_int32(first, last);
   assert(last - first >= count);
   std::string str(first, first + count);
-  first += ((-count % OSC_BLOB_MOD) + OSC_BLOB_MOD) % OSC_BLOB_MOD;
+  first += ((-count % OSC_MOD) + OSC_MOD) % OSC_MOD;
   assert(first <= last);
   return str;
 }
 
 int main (int argc, char *argv[]) {
-  std::string in("#bundle\0\0\0\0\1", 12);
+  std::string in("#bundle\0\0\0\0\0\0\0\0\1", 16);
   std::string::iterator it = in.begin();
   std::string c1 = get_string(it, in.end());
   printf("%s\n%d\n", c1.c_str(), (int) (it - in.begin()));
-  float c2 = get_float(it, in.end());
-  printf("%f\n%d\n", c2, (int) (in.size()));
+  timetag c2 = get_time(it, in.end());
+  printf("%.11Lf\n%d\n", (long double) c2.secs + (long double) c2.frac / (1ul << 32), (int) (it - in.begin()));
 }
