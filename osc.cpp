d6b806eb22cbe48cb4aa9102b72ea39ca59f47f1
Jaan Toots <jaantoots@gmail.com>
2018-05-18 18:16:35 +0100

    Improve comments

--- osc.cpp
+++ osc.cpp
@@ -7,9 +7,9 @@
 #define OSC_MOD 4
 #define NTP_DELTA 2208988800
 
+/* Get OSC_MOD chars as type T from Iter */
 template<class T, class Iter>
 T get_from_c (Iter &first, const Iter &last) {
-  // Get OSC_MOD chars as type T
   assert(last - first >= OSC_MOD);
   union mix32_t {
     T t;
@@ -18,42 +18,44 @@ T get_from_c (Iter &first, const Iter &last) {
   } m;
   copy_n(first, OSC_MOD, m.c);
   first += OSC_MOD;
+  // Convert from big endian to system
   m.u = be32toh(m.u);
   return m.t;
 }
 
+/* Get signed 32-bit integer from chars */
 template<class Iter>
 int32_t get_int32 (Iter &first, const Iter &last) {
-  // Get signed 32-bit integer from chars
   return get_from_c<int32_t>(first, last);
 }
 
+/* Get unsigned 32-bit integer from chars */
 template<class Iter>
 uint32_t get_uint32 (Iter &first, const Iter &last) {
-  // Get unsigned 32-bit integer from chars
   return get_from_c<uint32_t>(first, last);
 }
 
+/* Get timestamp from unsigned 64-bit NTP timestamp */
 template<class Iter>
 long double get_time (Iter &first, const Iter &last) {
-  // Get timestamp from unsigned 64-bit NTP timestamp
   uint32_t secs = get_uint32(first, last);
   uint32_t frac = get_uint32(first, last);
+  // `long double` *should* be 80-bit float
   long double t = secs + (long double) frac / (1ul << 32);
   t -= NTP_DELTA; // NTP time starts at 1 Jan 1900
   return t;
 }
 
+/* Get IEEE-754 32 bit float */
 template<class Iter>
 float get_float (Iter &first, const Iter &last) {
-  // Get IEEE-754 32 bit float
   assert(std::numeric_limits<float>::is_iec559);
   return get_from_c<float>(first, last);
 }
 
+/* Get a \0 terminated and padded string */
 template<class Iter>
 std::string get_string (Iter &first, const Iter &last) {
-  // Get a \0 terminated and padded string
   Iter end = std::find(first, last, '\0');
   std::string str(first, end);
   end += ((first - end) % OSC_MOD) + OSC_MOD;
@@ -62,9 +64,9 @@ std::string get_string (Iter &first, const Iter &last) {
   return str;
 }
 
+/* Get arbitrary length binary blob */
 template<class Iter>
 std::string get_blob (Iter &first, const Iter &last) {
-  // Get arbitrary length binary blob
   long count = get_int32(first, last);
   assert(last - first >= count);
   std::string str(first, first + count);
